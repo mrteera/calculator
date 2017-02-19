e0d0d3779cd323658a70ace724c6c78c28d9a0af
mrteera <inbox@mrteera.com>
2017-02-19 17:24:58 +0700

    Add postfix calculation into the calculator

--- test.sh
+++ test.sh
@@ -5,14 +5,26 @@ cp test.txt.infix test.txt && \
 cp output.good.infix output.good && \
 make clean && \
 make all && \
-echo "======== Infix ========" && \
-cat output.txt && \
-echo "======== Infix ========" && \
+cp output.txt output.txt.infix && \
 # Test prefix
 cp test.txt.prefix test.txt && \
 cp output.good.prefix output.good && \
 make clean && \
 make all && \
-echo "======== Prefix ========" && \
-cat output.txt && \
+cp output.txt output.txt.prefix && \
+# Test postfix
+cp test.txt.postfix test.txt && \
+cp output.good.postfix output.good && \
+make clean && \
+make all && \
+cp output.txt output.txt.postfix && \
+echo "======== Infix ========"
+cat output.txt.infix
+echo "======== Infix ========"
 echo "======== Prefix ========"
+cat output.txt.prefix
+echo "======== Prefix ========"
+echo "======== Postfix ========"
+cat output.txt.postfix
+echo "======== Postfix ========"
+rm -f output.txt.*
